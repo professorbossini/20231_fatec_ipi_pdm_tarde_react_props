c48fc9aaabcfde3ca9987c0ff6bc39a1acf169f8
Rodrigo Bossini <professorbossini@gmail.com>
2023-02-17 17:43:32 -0300

    apostila04: fim da secao 2.5
    - tres pedidos sendo exibidos
    - codigo replicado tres vezes
    - a aplicacao ainda nao faz uso de componentes
    - o props tambem nao esta sendo utilizado ainda

--- src/index.js
+++ src/index.js
@@ -1,17 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => (
+    <div className='container border rounded mt-2'>
+        <div className='row border-bottom m-2'>
+            <h1 className='display-5 text-center'>Seus pedidos</h1>
+        </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        <div
+            className='row'>
+            <div
+                className='col-sm-8 col-md-6 m-2'>
+            </div>
+            <div
+                className='card'>
+                <div
+                    className='card-header text-muted'>
+                        22/04/2021
+                </div>
+                <div
+                    className='card-body d-flex'>
+                    <div className='d-flex align-items-center'>
+                        <i className='fas fa-hdd fa-2x'></i>
+                    </div>
+                    <div
+                        className='flex-grow-1 ms-2 rounded border'>
+                        <h4 className='text-center'>SSD</h4>
+                        <p className='text-center'>SSD Kingston A400 - SATA</p>    
+                    </div>
+                </div>
+            </div>        
+        </div>
+
+        <div
+            className='row'>
+            <div
+                className='col-sm-8 col-md-6 m-2'>
+            </div>
+            <div
+                className='card'>
+                <div
+                    className='card-header text-muted'>
+                        20/04/2021
+                </div>
+                <div
+                    className='card-body d-flex'>
+                    <div className='d-flex align-items-center'>
+                        <i className='fas fa-book fa-2x'></i>
+                    </div>
+                    <div
+                        className='flex-grow-1 ms-2 rounded border'>
+                        <h4 className='text-center'>Livro</h4>
+                        <p className='text-center'>Concrete Mathematics - Donald Knuth</p>    
+                    </div>
+                </div>
+            </div>        
+        </div>
+
+        <div
+            className='row'>
+            <div
+                className='col-sm-8 col-md-6 m-2'>
+            </div>
+            <div
+                className='card'>
+                <div
+                    className='card-header text-muted'>
+                        21/01/2021
+                </div>
+                <div
+                    className='card-body d-flex'>
+                    <div className='d-flex align-items-center'>
+                        <i className='fas fa-laptop fa-2x'></i>
+                    </div>
+                    <div
+                        className='flex-grow-1 ms-2 rounded border'>
+                        <h4 className='text-center'>Notebook</h4>
+                        <p className='text-center'>Notebook Dell - 8Gb - i5</p>    
+                    </div>
+                </div>
+            </div>        
+        </div>
+
+    </div>
+)
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
