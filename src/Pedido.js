85439c64cde183dc3d17c79902b5d196454d3c68
Rodrigo Bossini <professorbossini@gmail.com>
2023-02-17 18:13:04 -0300

    apostila04: fim da secao 2.7
    - componente Pedido criado e utilizado 3 vezes
    - Uso basico do props ilustrado

---
+++ src/Pedido.js
@@ -0,0 +1,22 @@
+const Pedido = (props) => (
+    <div
+        className='card'>
+        <div
+            className='card-header text-muted'>
+            {props.data}
+        </div>
+        <div
+            className='card-body d-flex'>
+            <div className='d-flex align-items-center'>
+                <i className={props.icone}></i>
+            </div>
+            <div
+                className='flex-grow-1 ms-2 rounded border'>
+                <h4 className='text-center'>{props.titulo}</h4>
+                <p className='text-center'>{props.descricao}</p>
+            </div>
+        </div>
+    </div>
+)
+
+export default Pedido
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import Pedido from './Pedido'
 
 const App = () => (
     <div className='container border rounded mt-2'>
@@ -13,75 +14,40 @@ const App = () => (
             className='row'>
             <div
                 className='col-sm-8 col-md-6 m-2'>
-            </div>
-            <div
-                className='card'>
-                <div
-                    className='card-header text-muted'>
-                        22/04/2021
-                </div>
-                <div
-                    className='card-body d-flex'>
-                    <div className='d-flex align-items-center'>
-                        <i className='fas fa-hdd fa-2x'></i>
-                    </div>
-                    <div
-                        className='flex-grow-1 ms-2 rounded border'>
-                        <h4 className='text-center'>SSD</h4>
-                        <p className='text-center'>SSD Kingston A400 - SATA</p>    
-                    </div>
-                </div>
-            </div>        
+                    <Pedido 
+                        data='22/04/2023'
+                        icone="fas fa-hdd fa-2x"
+                        titulo="SSD"
+                        descricao="SSD Kingston A400 - SATA"
+                    />
+            </div>                    
         </div>
 
         <div
             className='row'>
             <div
                 className='col-sm-8 col-md-6 m-2'>
+                    <Pedido 
+                        data='20/04/2023'
+                        icone='fas fa-book fa-2x'
+                        titulo="Livro"
+                        descricao="Concrete Mathematics - Donald Knuth"
+                    />
             </div>
-            <div
-                className='card'>
-                <div
-                    className='card-header text-muted'>
-                        20/04/2021
-                </div>
-                <div
-                    className='card-body d-flex'>
-                    <div className='d-flex align-items-center'>
-                        <i className='fas fa-book fa-2x'></i>
-                    </div>
-                    <div
-                        className='flex-grow-1 ms-2 rounded border'>
-                        <h4 className='text-center'>Livro</h4>
-                        <p className='text-center'>Concrete Mathematics - Donald Knuth</p>    
-                    </div>
-                </div>
-            </div>        
+                   
         </div>
 
         <div
             className='row'>
             <div
                 className='col-sm-8 col-md-6 m-2'>
+                    <Pedido 
+                        data="21/01/2021"
+                        icone="fas fa-laptop fa-2x"
+                        titulo="Notebook"
+                        descricao="Notebook Dell - 8Gb - i5"
+                    />
             </div>
-            <div
-                className='card'>
-                <div
-                    className='card-header text-muted'>
-                        21/01/2021
-                </div>
-                <div
-                    className='card-body d-flex'>
-                    <div className='d-flex align-items-center'>
-                        <i className='fas fa-laptop fa-2x'></i>
-                    </div>
-                    <div
-                        className='flex-grow-1 ms-2 rounded border'>
-                        <h4 className='text-center'>Notebook</h4>
-                        <p className='text-center'>Notebook Dell - 8Gb - i5</p>    
-                    </div>
-                </div>
-            </div>        
         </div>
 
     </div>
