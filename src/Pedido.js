2f14b85fa4c94d0faf4d6702bbad4e09a832d231
Rodrigo Bossini <professorbossini@gmail.com>
2023-02-24 15:57:02 -0300

    apostila04: secao 2.8 encerrada
    - componente Cartao criado
    - aplicacao refatorada utilizando o componente Cartao
    - arquivo Pedido.js atualizado para que nao mais faca uso de classes card do Bootstrap (isso ainda nao esta na apostila)

--- src/Pedido.js
+++ src/Pedido.js
@@ -1,20 +1,13 @@
 const Pedido = (props) => (
     <div
-        className='card'>
-        <div
-            className='card-header text-muted'>
-            {props.data}
+        className='d-flex'>
+        <div className='d-flex align-items-center'>
+            <i className={props.icone}></i>
         </div>
         <div
-            className='card-body d-flex'>
-            <div className='d-flex align-items-center'>
-                <i className={props.icone}></i>
-            </div>
-            <div
-                className='flex-grow-1 ms-2 rounded border'>
-                <h4 className='text-center'>{props.titulo}</h4>
-                <p className='text-center'>{props.descricao}</p>
-            </div>
+            className='flex-grow-1 ms-2 rounded border'>
+            <h4 className='text-center'>{props.titulo}</h4>
+            <p className='text-center'>{props.descricao}</p>
         </div>
     </div>
 )
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import Pedido from './Pedido'
+import Cartao from './Cartao'
 
 const App = () => (
     <div className='container border rounded mt-2'>
@@ -14,12 +15,13 @@ const App = () => (
             className='row'>
             <div
                 className='col-sm-8 col-md-6 m-2'>
-                    <Pedido 
-                        data='22/04/2023'
-                        icone="fas fa-hdd fa-2x"
-                        titulo="SSD"
-                        descricao="SSD Kingston A400 - SATA"
-                    />
+                    <Cartao cabecalho="22/04/2021">
+                        <Pedido 
+                            icone="fas fa-hdd fa-2x"
+                            titulo="SSD"
+                            descricao="SSD Kingston A400 - SATA"
+                        />
+                    </Cartao>
             </div>                    
         </div>
 
@@ -27,12 +29,14 @@ const App = () => (
             className='row'>
             <div
                 className='col-sm-8 col-md-6 m-2'>
-                    <Pedido 
-                        data='20/04/2023'
-                        icone='fas fa-book fa-2x'
-                        titulo="Livro"
-                        descricao="Concrete Mathematics - Donald Knuth"
-                    />
+                    <Cartao
+                        cabecalho="20/04/2023">
+                        <Pedido 
+                            icone='fas fa-book fa-2x'
+                            titulo="Livro"
+                            descricao="Concrete Mathematics - Donald Knuth"
+                        />
+                    </Cartao>
             </div>
                    
         </div>
@@ -41,12 +45,14 @@ const App = () => (
             className='row'>
             <div
                 className='col-sm-8 col-md-6 m-2'>
-                    <Pedido 
-                        data="21/01/2021"
-                        icone="fas fa-laptop fa-2x"
-                        titulo="Notebook"
-                        descricao="Notebook Dell - 8Gb - i5"
-                    />
+                    <Cartao
+                        cabecalho="21/01/2021">
+                        <Pedido 
+                            icone="fas fa-laptop fa-2x"
+                            titulo="Notebook"
+                            descricao="Notebook Dell - 8Gb - i5"
+                        />
+                    </Cartao>
             </div>
         </div>
 
